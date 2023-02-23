e6e021424785847815bb9826e40a42557ddacb64
mateusoliiveira <teusolive@gmail.com>
2023-02-22 21:37:56 -0300

    feat: products show screen

---
+++ src/screens/ProductShowScreen.tsx
@@ -0,0 +1,88 @@
+import React from "react";
+import {
+  StyleSheet,
+  View,
+  Image,
+  FlatList,
+  useWindowDimensions,
+  Text,
+  ScrollView,
+  Pressable,
+} from "react-native";
+import products from "../data/products";
+
+export default function ProductShowScreen() {
+  const product = products[0];
+  const { width } = useWindowDimensions();
+  let screenWidth = width;
+
+  function addToCart() {
+    console.warn("added");
+  }
+
+  return (
+    <View>
+      <ScrollView>
+        <FlatList
+          data={product.images}
+          renderItem={({ item }) => (
+            <Image
+              source={{ uri: item }}
+              style={{ width: screenWidth, aspectRatio: 1 }}
+            />
+          )}
+          horizontal
+          showsHorizontalScrollIndicator={false}
+          pagingEnabled
+        />
+        <View style={{ padding: 20 }}>
+          <Text style={styles.title}>{product.name}</Text>
+          <Text style={styles.price}>
+            {product.price.toLocaleString("pt-BR", {
+              style: "currency",
+              currency: "BRL",
+            })}
+          </Text>
+          <Text style={styles.description}>{product.description}</Text>
+        </View>
+      </ScrollView>
+      <Pressable style={styles.button} onPress={addToCart}>
+        <Text style={styles.buttonText}>Adicionar ao carrinho</Text>
+      </Pressable>
+    </View>
+  );
+}
+
+const styles = StyleSheet.create({
+  title: {
+    fontSize: 34,
+    fontWeight: "500",
+    marginVertical: 10,
+  },
+  price: {
+    fontWeight: "500",
+    fontSize: 16,
+    letterSpacing: 1.5,
+  },
+  description: {
+    marginVertical: 10,
+    fontSize: 18,
+    lineHeight: 30,
+    fontWeight: "300",
+  },
+  button: {
+    position: "absolute",
+    backgroundColor: "#000",
+    bottom: 30,
+    width: "90%",
+    alignSelf: "center",
+    padding: 20,
+    borderRadius: 100,
+    alignItems: "center",
+  },
+  buttonText: {
+    color: "#fff",
+    fontWeight: "500",
+    fontSize: 16,
+  },
+});
